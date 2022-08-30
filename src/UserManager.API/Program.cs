1adafe831180f9192c14cc7873e0dcf98c12a3da
Vinicius Oliveira <viniciusoliverrs@gmail.com>
2022-08-30 20:02:23 -0300

    fix: Bug for user update

--- src/UserManager.API/Program.cs
+++ src/UserManager.API/Program.cs
@@ -23,6 +23,7 @@ var autoMapperConfig = new MapperConfiguration(cfg =>
 {
     cfg.CreateMap<User, UserDTO>().ReverseMap();
     cfg.CreateMap<CreateUserViewModel,UserDTO>().ReverseMap();
+    cfg.CreateMap<UpdatedUserViewModel,UserDTO>().ReverseMap();
 
 });
 builder.Services.AddSingleton(autoMapperConfig.CreateMapper());
